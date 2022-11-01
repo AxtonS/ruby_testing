6e2e318d969e7723b85496c478ef509e0c881937
Axton Smith <axtonsmith@gmail.com>
2022-11-01 19:30:51 -0400

    Finish ruby monstas RSpec tutorial

---
+++ spec/birthday_spec.rb
@@ -0,0 +1,51 @@
+require 'date'
+
+def leap_year?(year)
+  year % 400 == 0 or year % 100 != 0 and year % 4 == 0
+end
+
+class User
+  def initialize(name, birthday)
+    @name = name
+    @birthday = birthday
+  end
+
+  def name
+    @name
+  end
+
+  def born_in_leap_year?
+    leap_year?(Date.parse(@birthday).year)
+  end
+end
+
+describe User do
+  subject { User.new("Francisca", "#{year}-01-01") }
+  
+  context "born in 2001" do    
+    let(:year) { 2001 }
+    it { should be_named("Francisca") }
+    it { should_not be_born_in_leap_year }
+  end
+
+  context "born in 1900" do
+    let(:year) { 1900 }
+    it { should_not be_born_in_leap_year }
+  end
+
+  context "born in 2000" do
+    let(:year) { 2000 }
+    it { should be_born_in_leap_year }
+  end
+
+  context "born in 2004" do
+    let(:year) { 2004 }
+    it { should be_born_in_leap_year }
+  end
+end
+
+RSpec::Matchers.define(:be_named) do |expected|
+  match do |object|
+    object.name == expected
+  end
+end
